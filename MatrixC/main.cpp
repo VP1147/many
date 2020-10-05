76d48e5b2e5b0208e348e63b650bd60ef146099b
Vinícius Pavão <pavao364@gmail.com>
2020-10-05 16:55:40 -0400

    Updated MatrixC

--- MatrixC/main.cpp
+++ MatrixC/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 void Det(void){
+	srand((unsigned) time(NULL));
 	int M[2][2];
 	for(int i=0; i<2; i++){
 		for(int e=0; e<2; e++){
@@ -13,9 +15,9 @@ void Det(void){
 	cout<<"|"<<M[0][0]<<" "<<M[0][1]<<"|\n";
 	cout<<"|"<<M[1][0]<<" "<<M[1][1]<<"|"<<" = ";
 	cin>>r; if(r == d) {
-		cout<<"\nCerto";
+		cout<<"\nCerto\n";
 	} else {
-		cout<<"\nO certo seria "<<d;
+		cout<<"\nO certo seria "<<d<<"\n";
 	}
 }
 	
@@ -26,8 +28,10 @@ int main(void) {
 	cout<<"<< MatrixC >>\n";
 	cout<<"1 - Determinantes\n";
 	cin>>select;
-	switch(select) {
-		case 1: Det();
-		break;
+	bool loop = true; while(loop = true) {
+		switch(select) {
+			case 1: Det();
+			break;
+		}
 	}
 }
